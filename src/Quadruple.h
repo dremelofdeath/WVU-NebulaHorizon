7a9d0c304a29fe19582196c7fc610b144735b387
zack <zack@b35edebe-94b0-4c99-81e8-a5a7506291ad>
2009-11-21 09:17:51 +0000

    Adding the Tuple, Triple, and Quadruple template classes for magical amazing purposes. :D

---
+++ src/Quadruple.h
@@ -0,0 +1,42 @@
+/*
+ *  Quadruple.h
+ *  Nebula Horizon
+ *
+ *  Created by Zachary Murray on 11/21/09.
+ *
+ */
+
+#include "Triple.h"
+
+#ifndef NHZ_QUADRUPLE_H
+#define NHZ_QUADRUPLE_H
+
+template <class T>
+class Quadruple : public Triple<T> {
+    public:
+        Quadruple(T first, T second, T third, T fourth)
+        : Triple<T>(first, second, third) {
+            initialize(first, second, third, fourth);
+        }
+        T getFourth() {
+            return _fourth;
+        }
+        virtual T operator[](int i) {
+            if(i == 3) {
+                return getFourth();
+            }
+            return Triple<T>::operator[](i);
+        }
+    protected:
+        virtual int getMaxCapacity() const {
+            return 4;
+        }
+        void initialize(T first, T second, T third, T fourth) {
+            Triple<T>::initialize(first, second, third);
+            _fourth = fourth;
+        }
+    private:
+        T _fourth;
+};
+
+#endif
---
+++ src/Triple.h
@@ -0,0 +1,42 @@
+/*
+ *  Triple.h
+ *  Nebula Horizon
+ *
+ *  Created by Zachary Murray on 11/21/09.
+ *
+ */
+
+#include "Tuple.h"
+
+#ifndef NHZ_TRIPLE_H
+#define NHZ_TRIPLE_H
+
+template <class T>
+class Triple : public Tuple<T> {
+    public:
+        Triple(T first, T second, T third)
+        : Tuple<T>(first, second) {
+            initialize(first, second, third);
+        }
+        T getThird() {
+            return _third;
+        }
+        virtual T operator[](int i) {
+            if(i == 2) {
+                return getThird();
+            }
+            return Tuple<T>::operator[](i);
+        }
+    protected:
+        virtual int getMaxCapacity() const {
+            return 3;
+        }
+        void initialize(T first, T second, T third) {
+            Tuple<T>::initialize(first, second);
+            _third = third;
+        }
+    private:
+        T _third;
+};
+
+#endif
