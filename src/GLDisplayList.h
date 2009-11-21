3ff9730f2f29b66d8189ae07276f3c7e006dd160
zack <zack@b35edebe-94b0-4c99-81e8-a5a7506291ad>
2009-11-21 23:43:54 +0000

    Adding a class that makes GL display lists play well with the Renderable class.

---
+++ src/GLDisplayList.h
@@ -0,0 +1,33 @@
+/*
+ *  GLDisplayList.h
+ *  Nebula Horizon
+ *
+ *  Created by Zachary Murray on 11/21/09.
+ *
+ */
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+
+#include "Renderable.h"
+
+#ifndef NHZ_GLDISPLAYLIST_H
+#define NHZ_GLDISPLAYLIST_H
+
+class GLDisplayList : public Renderable {
+    public:
+        GLDisplayList();
+        GLDisplayList(GLuint list);
+        void render();
+        GLuint getList();
+    protected:
+        void initialize();
+        void initialize(GLuint list);
+    private:
+        GLuint _list;
+};
+
+#endif
