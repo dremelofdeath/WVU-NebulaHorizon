411fad75d1641dabb86828a0594db2a6cbec014c
zack <zack@b35edebe-94b0-4c99-81e8-a5a7506291ad>
2009-11-21 09:18:27 +0000

    Adding some new methods for managing the vertex lists in MeshLoader.

--- src/MeshLoader.cpp
+++ src/MeshLoader.cpp
@@ -14,7 +14,8 @@ void MeshLoader::processVertex(double x, double y, double z) {
 }
 
 void MeshLoader::processVertex(double x, double y, double z, double w) {
-    // default implementation does nothing
+    // default implementation simply adds the vertex to the list
+    addVertexToList(x, y, z, w);
 }
 
 void MeshLoader::processTextureVertex(double u) {
@@ -26,15 +27,17 @@ void MeshLoader::processTextureVertex(double u, double v) {
 }
 
 void MeshLoader::processTextureVertex(double u, double v, double w) {
-    // default implementation does nothing
+    // default implementation simply adds the texture vertex to the list
+    addTextureVertexToList(u, v, w);
 }
 
 void MeshLoader::processVertexNormal(double i, double j, double k) {
-    // default implementation does nothing
+    // default implementation simply adds the normal to the list
+    addVertexNormalToList(i, j, k);
 }
 
 void MeshLoader::processPoint(const std::vector<int>* vertices) {
-    // default implementation does nothing
+    // default implementation does nothing but check for errors
     if(vertices == 0) {
         throw std::invalid_argument("vertices");
     } else if(vertices->size() < 1) {
@@ -83,4 +86,28 @@ void MeshLoader::processFace(const std::vector<int>* v,
     if(vn != 0 && v->size() != vn->size()) {
         throw std::length_error("vn");
     }
+}
+
+void MeshLoader::addVertexToList(double x, double y, double z, double w) {
+    _vertexList.push_back(new Quadruple<double>(x, y, z, w));
+}
+
+void MeshLoader::addTextureVertexToList(double u, double v, double w) {
+    _textureVertexList.push_back(new Triple<double>(u, v, w));
+}
+
+void MeshLoader::addVertexNormalToList(double i, double j, double k) {
+    _vertexNormalList.push_back(new Triple<double>(i, j, k));
+}
+
+Quadruple<double>* MeshLoader::getVertex(size_t i) {
+    return _vertexList[(std::vector<Quadruple<double>*>::size_type)i];
+}
+
+Triple<double>* MeshLoader::getTextureVertex(size_t i) {
+    return _textureVertexList[(std::vector<Triple<double>*>::size_type)i];
+}
+
+Triple<double>* MeshLoader::getVertexNormal(size_t i) {
+    return _vertexNormalList[(std::vector<Triple<double>*>::size_type)i];
 }
