4cf481e9f89a26b2559d49fca3b54558f4288152
Zachary Murray <709873+dremelofdeath@users.noreply.github.com>
2009-11-22 21:59:35 +0000

    It spins and loads! :D

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,28 @@
 #endif
 
 #include "RenderQueue.h"
+#include "MeshLoader.h"
+#include "WavefrontGLMeshLoader.h"
 
 static GLsizei winWidth = 800, winHeight = 600;
 static int window_id_main = 0;
 
 void display() {
+    static double angle = 0.0;
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+    glTranslated(0.0, -1.0, -13.0);
+    glRotated(angle, 0.0, 1.0, 0.0);
+    angle += 0.25;
+    glScalef(0.75, 0.75, 0.75);
+    glTranslated(0.0, 0.0, 1.0);
     RenderQueue::getInstance()->render();
+    glPopMatrix();
     glutSwapBuffers();
 }
 
 void idle() {
+    glutPostRedisplay();
 }
 
 void handle_special_keys(int key, int x, int y) {
@@ -37,12 +48,21 @@ void handle_special_keys(int key, int x, int y) {
 void update_projection(GLsizei w, GLsizei h) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(90.0, 1.0, 0.1, 100);
+    gluPerspective(45.0, (double)w/(double)h, 0.1, 100);
+}
+
+static void reshape(GLsizei w, GLsizei h) {
+    winWidth = w;
+    winHeight = h;
+    update_projection(w, h);
+    glViewport(0, 0, w, h);
+    glutPostRedisplay();
 }
 
 void create_callbacks() {
     glutDisplayFunc(&display);
     glutIdleFunc(&idle);
+    glutReshapeFunc(&reshape);
     glutSpecialFunc(&handle_special_keys);
 }
 
@@ -69,6 +89,14 @@ void init_opengl() {
     gluLookAt(0.0, 0.75, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
+void load_viper() {
+    std::ifstream viper_file("meshes/viper.obj");
+    MeshLoader* loader = new WavefrontGLMeshLoader;
+    Renderable* viper_model = loader->loadMesh(viper_file);
+    RenderQueue::getInstance()->enqueue(*viper_model);
+    delete loader;
+}
+
 int main(int argc, char *argv[]) {
     glutInit(&argc, argv);
     const char *title = "Nebula Horizon";
@@ -76,6 +104,7 @@ int main(int argc, char *argv[]) {
     atexit(&exit_callback);
     create_callbacks();
     init_opengl();
+    load_viper();
     glutMainLoop();
     return 0;
 }
