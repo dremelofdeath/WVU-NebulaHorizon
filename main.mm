219efb62a0f71d7c3b11539b67c14917fce3860e
zack <zack@b35edebe-94b0-4c99-81e8-a5a7506291ad>
2009-11-24 06:42:57 +0000

    This is the new Xcode project. This one actually builds an app bundle. Horray for Objective-C++!

---
+++ main.mm
@@ -0,0 +1,14 @@
+//
+//  main.mm
+//  Nebula Horizon
+//
+//  Created by Zachary Murray on 11/23/09.
+//
+
+#import <Cocoa/Cocoa.h>
+#include <GLUT/glut.h>
+
+int main(int argc, char *argv[]) {
+	glutInit(&argc, argv);
+    return NSApplicationMain(argc,  (const char **) argv);
+}
