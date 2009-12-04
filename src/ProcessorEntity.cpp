4a26e22736e532751b1025c18cd3fca3eacf53fb
zack <zack@b35edebe-94b0-4c99-81e8-a5a7506291ad>
2009-12-04 03:59:57 +0000

    Adding the ProcessorEntity class, a class that is a child of Renderable but renders nothing. It is intended to simplify the use of things controlling state, such as a timer that spawns enemies every five seconds, or perhaps something that generates random terrain.

---
+++ src/ProcessorEntity.cpp
@@ -0,0 +1,18 @@
+/*
+ *  ProcessorEntity.cpp
+ *  Nebula Horizon
+ *
+ *  Created by Zachary Murray on 12/3/09.
+ *
+ */
+
+#include "ProcessorEntity.h"
+
+void ProcessorEntity::render() const {
+    // never does anything -- only processes time and events
+}
+
+void ProcessorEntity::idle(int elapsed) {
+    // default implementation does nothing but call the superclass version
+    Renderable::idle(elapsed);
+}
---
+++ src/ProcessorEntity.h
@@ -0,0 +1,20 @@
+/*
+ *  ProcessorEntity.h
+ *  Nebula Horizon
+ *
+ *  Created by Zachary Murray on 12/3/09.
+ *
+ */
+
+#ifndef NHZ_PROCESSORENTITY_H
+#define NHZ_PROCESSORENTITY_H
+
+#include "Renderable.h"
+
+class ProcessorEntity : public Renderable {
+    public:
+        void render() const;
+        virtual void idle(int elapsed) = 0;
+};
+
+#endif
