45a47204642001a116a03c88f37d37861caed436
Zachary Murray <709873+dremelofdeath@users.noreply.github.com>
2009-12-04 00:56:57 +0000

    Some minor bugfixes with the player and the keyboard manager.

--- src/KeyboardManager.cpp
+++ src/KeyboardManager.cpp
@@ -7,6 +7,7 @@
  */
 
 #include <cctype>
+#include <GLUT/glut.h>
 #include "KeyboardManager.h"
 
 KeyboardManager* KeyboardManager::_singleton = 0;
@@ -26,13 +27,37 @@ bool KeyboardManager::isKeyDown(unsigned char key) {
     return _keys[tolower(key)];
 }
 
+bool KeyboardManager::isSpecialKeyDown(int key) {
+    bool ret = false;
+    if(key >= GLUT_KEY_F1 && key <= GLUT_KEY_F12) {
+        ret = _functionKeys[key - GLUT_KEY_F1];
+    } else if(key >= GLUT_KEY_LEFT && key <= GLUT_KEY_INSERT) {
+        ret = _directionalKeys[key - GLUT_KEY_LEFT];
+    }
+    return ret;
+}
+
 void KeyboardManager::updateKeyState(unsigned char key, bool isDown) {
     _keys[tolower(key)] = isDown;
+}
+
+void KeyboardManager::updateSpecialKeyState(int key, bool isDown) {
+    if(key >= GLUT_KEY_F1 && key <= GLUT_KEY_F12) {
+        _functionKeys[key - GLUT_KEY_F1] = isDown;
+    } else if(key >= GLUT_KEY_LEFT && key <= GLUT_KEY_INSERT) {
+        _directionalKeys[key - GLUT_KEY_LEFT] = isDown;
+    }
 }
 
 void KeyboardManager::initialize() {
     int i = 0;
     for(i = 0; i <= UCHAR_MAX; i++) {
         _keys[i] = false;
+    }
+    for(i = 0; i < 9; i++) {
+        _functionKeys[i] = false;
+    }
+    for(i = 0; i < 12; i++) {
+        _directionalKeys[i] = false;
     }
 }
