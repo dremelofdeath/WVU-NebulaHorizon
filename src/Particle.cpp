9da64e86800a3ba78cf853ff8d0a3c013203c0e4
Zachary Murray <709873+dremelofdeath@users.noreply.github.com>
2010-03-31 02:24:12 +0000

    Updating the particle system to use GL point sprites (if supported) and calls glPointParameter* to set distance attenuation for a very nice effect.

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -8,11 +8,15 @@
 
 #include "Particle.h"
 
+#include <cmath>
+
 #ifdef __APPLE__
 #include <OpenGL/glext.h>
 #else
 #include <GL/glext.h>
 #endif
+
+#include "nhz_common.h"
 
 GLuint Particle::_particleID = 0;
 
@@ -31,7 +35,7 @@ void Particle::render() const {
   glColor4f(_r, _g, _b, _life);
   doTranslation();
   glScalef(scaleFactor, scaleFactor, scaleFactor);
-  glPointSize(8.0*_life);
+  glPointSize(8.0f*_life);
   renderParticle();
 }
 
@@ -51,11 +55,9 @@ void Particle::idle(const int elapsed) {
     _particleID = glGenLists(1);
     glNewList(_particleID, GL_COMPILE);
     //glutSolidSphere(1.0, 8, 8);
-    glEnable(GL_POINT_SPRITE);
     glBegin(GL_POINTS);
     glVertex3i(0, 0, 0);
     glEnd();
-    glDisable(GL_POINT_SPRITE);
     glEndList();
   }
 }
@@ -67,8 +69,11 @@ void Particle::renderParticle() const {
 }
 
 void Particle::initialize(ParticleFountain* parent) {
-  Movable::initialize(parent->getX()+rand()%100/500.0-0.1,
-      parent->getY()+rand()%100/500.0-0.1,
+  double xmod = rand()%100/500.0-0.1;
+  double angle = std::acos(10.0*xmod);
+  Movable::initialize(parent->getX()+xmod,
+      //parent->getY()+rand()%100/500.0-0.1,
+      parent->getY()+(rand()%100/500.0-0.1)*std::sin(angle),
       parent->getZ()+rand()%100/500.0-0.1);
   _parent = parent;
   _life = 1.0f;
@@ -78,6 +83,7 @@ void Particle::initialize(ParticleFountain* parent) {
   _b = _parent->getBlue();
   _xVelocity = _parent->getXSpeed()+(float)((rand()%60)-30.0f)/155;
   _yVelocity = _parent->getYSpeed()+(float)((rand()%60)-30.0f)/155;
-  _zVelocity = _parent->getZSpeed()+(float)((rand()%60)-0.0f)/32;
+  //_zVelocity = _parent->getZSpeed()+(float)((rand()%60)-0.0f)/32;
+  _zVelocity = _parent->getZSpeed()+(float)((rand()%60)-0.0f)/25;
 }
 
