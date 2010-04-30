1a1fbb9546995922fe069f4ccd4fa1311b600f19
Zachary Murray <709873+dremelofdeath@users.noreply.github.com>
2010-04-30 06:22:56 +0000

    Incredibly large changes to nhz_common. The header has a lot more GL procs now. There's also a new macro, NHZAUTOLOG, which, when _DEBUG is defined, will automagically print out the error logs from shader compiles and program links.

--- src/nhz_common.cpp
+++ src/nhz_common.cpp
@@ -1,10 +1,33 @@
 #include <cstring>
+#include <string>
 
 #include "nhz_common.h"
 
 // wglGetProcAddress function prototypes
 #ifdef WIN32
 PFNGLPOINTPARAMETERFVPROC NHZ_glPointParameterfvARB = NULL;
+PFNGLCREATESHADERPROC NHZ_glCreateShader = NULL;
+PFNGLATTACHSHADERPROC NHZ_glAttachShader = NULL;
+PFNGLSHADERSOURCEPROC NHZ_glShaderSource = NULL;
+PFNGLGETSHADERIVPROC NHZ_glGetShaderiv = NULL;
+PFNGLCOMPILESHADERPROC NHZ_glCompileShader = NULL;
+PFNGLDETACHSHADERPROC NHZ_glDetachShader = NULL;
+PFNGLDELETESHADERPROC NHZ_glDeleteShader = NULL;
+PFNGLGETSHADERINFOLOGPROC NHZ_glGetShaderInfoLog = NULL; 
+PFNGLCREATEPROGRAMPROC NHZ_glCreateProgram = NULL;
+PFNGLLINKPROGRAMPROC NHZ_glLinkProgram = NULL;
+PFNGLGETPROGRAMIVPROC NHZ_glGetProgramiv = NULL;
+PFNGLUSEPROGRAMPROC NHZ_glUseProgram = NULL;
+PFNGLDELETEPROGRAMPROC NHZ_glDeleteProgram = NULL;
+PFNGLGETPROGRAMINFOLOGPROC NHZ_glGetProgramInfoLog = NULL; 
+PFNGLPROGRAMPARAMETERIEXTPROC NHZ_glProgramParameteri = NULL;
+PFNGLGETATTRIBLOCATIONPROC NHZ_glGetAttribLocation = NULL;
+PFNGLENABLEVERTEXATTRIBARRAYPROC NHZ_glEnableVertexAttribArray = NULL;
+PFNGLDISABLEVERTEXATTRIBARRAYPROC NHZ_glDisableVertexAttribArray = NULL;
+PFNGLVERTEXATTRIBPOINTERPROC NHZ_glVertexAttribPointer = NULL;
+PFNGLGENBUFFERSPROC NHZ_glGenBuffers = NULL;
+PFNGLBINDBUFFERPROC NHZ_glBindBuffer = NULL;
+PFNGLBUFFERDATAPROC NHZ_glBufferData = NULL;
 #endif
 
 static char *gl_extensions_supported = NULL;
@@ -21,6 +44,44 @@ int gl_has_extension(const char *extension) {
   }
   ret = std::strstr(gl_extensions_supported, extension) != NULL;
   return ret;
+}
+
+int gl_supports_version(const double ver) {
+  static double _gl_version_id = 0.0;
+  if(_gl_version_id == 0.0) {
+    _gl_version_id = atof((const char *)glGetString(GL_VERSION));
+  }
+  return ver <= _gl_version_id;
+}
+
+void gl_print_shader_infolog(GLuint shader) {
+  int infologLength = 0;
+  int charsWritten  = 0;
+  char *infoLog;
+
+  glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infologLength);
+
+  if(infologLength > 0) {
+    infoLog = (char *)malloc(infologLength);
+    glGetShaderInfoLog(shader, infologLength, &charsWritten, infoLog); 
+    NHZ_ERR("%s\n", infoLog);
+    free(infoLog);
+  }
+}
+
+void gl_print_program_infolog(GLuint program) {
+  int infologLength = 0;
+  int charsWritten  = 0;
+  char *infoLog;
+
+  glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infologLength);
+
+  if(infologLength > 0) {
+    infoLog = (char *)malloc(infologLength);
+    glGetProgramInfoLog(program, infologLength, &charsWritten, infoLog); 
+    NHZ_ERR("%s\n", infoLog);
+    free(infoLog);
+  }
 }
 
 char *_nhzgeterr() {
@@ -51,12 +112,49 @@ wchar_t *_nhzgetwerr() {
 
 #define IF_EXTENSION_IS_SUPPORTED(ext) \
   if(!gl_has_extension( ext )) { \
-    NHZ_ERR( ext " is not supported; ignoring\n"); \
+    NHZ_ERR( ext " is not supported; extension load aborted\n"); \
+  } else \
+
+#define IF_GL_VERSION_IS_SUPPORTED(ver, whatfor) \
+  if(!gl_supports_version( ver )) { \
+    NHZ_ERR("OpenGL version " #ver " not supported; ignoring " whatfor "\n"); \
   } else \
 
 void nhz_load_all_gl_procs() {
   IF_EXTENSION_IS_SUPPORTED("GL_ARB_point_parameters") {
     NHZPROCLOAD(PFNGLPOINTPARAMETERFVARBPROC, glPointParameterfvARB);
+  }
+  IF_GL_VERSION_IS_SUPPORTED(2.0, "shaders") {
+    NHZPROCLOAD(PFNGLCREATESHADERPROC, glCreateShader);
+    NHZPROCLOAD(PFNGLATTACHSHADERPROC, glAttachShader);
+    NHZPROCLOAD(PFNGLSHADERSOURCEPROC, glShaderSource);
+    NHZPROCLOAD(PFNGLGETSHADERIVPROC, glGetShaderiv);
+    NHZPROCLOAD(PFNGLCOMPILESHADERPROC, glCompileShader);
+    NHZPROCLOAD(PFNGLDETACHSHADERPROC, glDetachShader);
+    NHZPROCLOAD(PFNGLDELETESHADERPROC, glDeleteShader);
+    NHZPROCLOAD(PFNGLGETSHADERINFOLOGPROC, glGetShaderInfoLog); 
+  }
+  IF_GL_VERSION_IS_SUPPORTED(2.0, "shader programs") {
+    NHZPROCLOAD(PFNGLCREATEPROGRAMPROC, glCreateProgram);
+    NHZPROCLOAD(PFNGLLINKPROGRAMPROC, glLinkProgram);
+    NHZPROCLOAD(PFNGLGETPROGRAMIVPROC, glGetProgramiv);
+    NHZPROCLOAD(PFNGLUSEPROGRAMPROC, glUseProgram);
+    NHZPROCLOAD(PFNGLDELETEPROGRAMPROC, glDeleteProgram);
+    NHZPROCLOAD(PFNGLGETPROGRAMINFOLOGPROC, glGetProgramInfoLog); 
+    NHZPROCLOAD(PFNGLPROGRAMPARAMETERIEXTPROC, glProgramParameteri);
+  }
+  IF_GL_VERSION_IS_SUPPORTED(2.0, "shader attributes") {
+    NHZPROCLOAD(PFNGLGETATTRIBLOCATIONPROC, glGetAttribLocation);
+    NHZPROCLOAD(PFNGLENABLEVERTEXATTRIBARRAYPROC,
+                glEnableVertexAttribArray);
+    NHZPROCLOAD(PFNGLDISABLEVERTEXATTRIBARRAYPROC,
+                glDisableVertexAttribArray);
+    NHZPROCLOAD(PFNGLVERTEXATTRIBPOINTERPROC, glVertexAttribPointer);
+  }
+  IF_GL_VERSION_IS_SUPPORTED(1.5, "buffer objects") {
+    NHZPROCLOAD(PFNGLGENBUFFERSPROC, glGenBuffers);
+    NHZPROCLOAD(PFNGLBINDBUFFERPROC, glBindBuffer);
+    NHZPROCLOAD(PFNGLBUFFERDATAPROC, glBufferData);
   }
 }
 
